0fc46076ba98cf2eb2d66b34343c3273ea68e77d
Azizo1212 <azizdali530@gmail.com>
2022-12-11 18:06:17 +0100

    2-export_to_JSON.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Export to csv format"""
+""" Api """
 
 if __name__ == "__main__":
     import csv
---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Api """
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+    user = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
+    t_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(userId)
+    name = requests.get(user).json()
+    todos = requests.get(t_url).json()
+
+    with open('{}.json'.format(userId), 'w') as json_file:
+        tasks = []
+        for t in todos:
+            tasks.append({"task": t.get("title"),
+                          "completed": t.get("completed"),
+                          "username": name.get("username")})
+
+        data = {"{}".format(userId): tasks}
+        json.dump(data, json_file)
